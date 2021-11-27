4bf59686d1a4cca38f6a2ad1855e279ea2edbb82
umakrishnapavan <93900825+umakrishnapavan@users.noreply.github.com>
2021-11-27 11:55:00 +0530

    Create avg.c

---
+++ avg.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,b,c,avg;
+    printf("to be find average");
+    scanf("%f",&a);
+    scanf("%f",&b);
+    scanf("%f",&c);
+    avg=(a+b+c)/3;
+    printf("%f",avg);
+    
+}
+
+    return 0;
+}
