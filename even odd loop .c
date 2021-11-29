7f8616d7b66660978cbcda53bc1218820a211f79
umakrishnapavan <93900825+umakrishnapavan@users.noreply.github.com>
2021-11-29 12:04:21 +0530

    Update even odd loop .c

--- even odd loop .c
+++ even odd loop .c
@@ -5,14 +5,14 @@ int main()
 {
     int a;
     
-    printf("to find the even numbers \n");
+    printf("to find the  numbers\n");
     for(a=1;a<=20;a++)
     {
         if(a%2==0)
-        printf("even\n%d",a);
+        printf("even%d\n",a);
         else
         
-        printf("odd\n%d",a);
+        printf("odd%d\n",a);
     }
 
     return 0;
