9f17725b1e969893cce4c288dcfc3315a53a0517
umakrishnapavan <93900825+umakrishnapavan@users.noreply.github.com>
2022-02-15 09:16:43 +0530

    Create no arg with return.c

---
+++ no arg with return.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int sum()
+{
+    int i,j,s;
+    printf("enter your number");
+    scanf("%d %d"&i,&j);
+    s=i+j;
+    return s;
+}
+int main()
+{
+    int y;
+    y=sum()
+    printf("%d",y);
+}
